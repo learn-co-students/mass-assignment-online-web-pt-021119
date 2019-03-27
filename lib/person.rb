31f20e480b416559e518643a7c93c87c5440cb82
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-27 02:13:37 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
+    # iterate through attributes hash and create instance variable, k, equal to v
     attributes.each {|k, v| self.send(("#{k}="), v)}
   end
   
